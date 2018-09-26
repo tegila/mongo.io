f3d8b1ee1775b4e0ca53a1dedcc7c18550c6f677
Edson Tégila Gonçalves <etegila@gmail.com>
2018-09-26 17:15:00 -0300

    docker adjusts

--- server/index.js
+++ server/index.js
@@ -10,12 +10,12 @@ const socket = require('./socket');
 const mongo = require('./mongo');
 
 const options = {
-  key: fs.readFileSync(path.resolve(__dirname, '../sample/cert.pem')),
-  cert: fs.readFileSync(path.resolve(__dirname, '../sample/cert.crt'))
+  key: fs.readFileSync(path.resolve(__dirname, './sample/cert.pem')),
+  cert: fs.readFileSync(path.resolve(__dirname, './sample/cert.crt'))
 };
 const port = process.argv[3] || 3000;
 
-const url = `mongodb://127.0.0.1:27017/`
+const url = `mongodb://mongo:27017/`
 
 mongo.enable(url, () => {
   socket.enable(options, port);
