34ef2d5d46446eb283a1c23f650eff2d84225031
Guilherme Silva <guilhermefos.developer@gmail.com>
2018-10-01 14:14:58 -0300

    Come back changes

--- client/queryBuilder/index.js
+++ client/queryBuilder/index.js
@@ -1,25 +1,39 @@
 const find = require('./find');
+//const remove = require('./remove');
 
-module.exports = () => {
-  return {
-    queryBuilder: (collection) => {
-      const load = {
-        transaction: {
-          collection,
-          payload: {}
-        }
-      }
-
-      if (this instanceof queryBuilder) {
-        return this.queryBuilder;
-      } else {
-        const _current = new queryBuilder();
-        _current.transaction = { collection };
-        return _current;
-      }
-    },
-    find: (query) => {
-      return find(query);
-    }
+const load = { 
+  transaction: {
+    collection,
+    payload: {}
   }
-}
+};
+const queryBuilder = (collection) => {
+  const load = { 
+    transaction: {
+      collection,
+      payload: {}
+    }
+  };
+
+
+
+  if (this instanceof queryBuilder) {
+    console.log('instanceOf');
+    return this.queryBuilder;
+  } else {
+    console.log('new');
+    const _current = new queryBuilder();
+    _current.transaction = { collection };
+    return _current;
+  }  
+};
+
+  queryBuilder.prototype.remove = (order) => {
+    Object.assign(load.payload, { order });
+    return this;
+  };
+  
+  queryBuilder.prototype.find = (query) => {
+    return find(query);
+  };
+module.exports = queryBuilder;
