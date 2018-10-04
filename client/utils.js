07b74996aee2e5ecf961de9005c14af4e4ef62ac
Guilherme Silva <guilhermefos.developer@gmail.com>
2018-10-04 11:06:41 -0300

    Refactor utils.js to use default Node module.exports

--- client/utils.js
+++ client/utils.js
@@ -1,22 +1,22 @@
-
-/* https://developer.mozilla.org/en-US/docs/Web/API/TextEncoder  */
-export const str2ab = (str) => {
-  const buf = new Uint8Array(str.length); // 2 bytes for each char
-  for (let i = 0; i < str.length; i += 1) {
-    buf[i] = str.charCodeAt(i);
-  }
-  return buf;
-}
-
-// https://stackoverflow.com/questions/12075927/serialization-of-regexp
-export const __parse_regex__ = (obj) => {
-  var key, value;
-  for (key in obj) {
-    value = obj[key];
-    if (value !== null && value instanceof RegExp) {
-      obj[key] = ("__REGEXP " + value.toString());
-    } else if (typeof value === 'object') {
-      __parse_regex__(value);
+module.exports = () => ({
+  /* https://developer.mozilla.org/en-US/docs/Web/API/TextEncoder  */
+  str2ab: (str) => {
+    const buf = new Uint8Array(str.length); // 2 bytes for each char
+    for (let i = 0; i < str.length; i += 1) {
+      buf[i] = str.charCodeAt(i);
+    }
+    return buf;
+  },
+  /* https://stackoverflow.com/questions/12075927/serialization-of-regexp */
+  __parse_regex__: (obj) => {
+    var key, value;
+    for (key in obj) {
+      value = obj[key];
+      if (value !== null && value instanceof RegExp) {
+        obj[key] = ("__REGEXP " + value.toString());
+      } else if (typeof value === 'object') {
+        __parse_regex__(value);
+      }
     }
   }
-}
+});
