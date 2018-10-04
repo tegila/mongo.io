4d1e663d2f0983d122cf3ab15703e67c2aef88a5
Guilherme Silva <guilhermefos.developer@gmail.com>
2018-10-04 12:42:20 -0300

    Testing change utils const name

--- client/auth.js
+++ client/auth.js
@@ -1,9 +1,9 @@
 const nacl = require('tweetnacl');
 const util = require('tweetnacl-util');
 const querystring = require('querystring');
-const utils = require('./utils');
+const utility = require('./utils');
 
-console.log('TYPEOF UTILS', typeof utils.str2ab(new Date().toString()));
+console.log('TYPEOF UTILS', typeof utility);
 
 
 const enc = util.encodeBase64;
@@ -32,7 +32,7 @@ const self = module.exports = {
     self.init_keychain(secretKey);
 
     const message = new Date().toString();
-    const signature = nacl.sign.detached(utils.str2ab(message), keypair.secretKey);
+    const signature = nacl.sign.detached(utility.str2ab(message), keypair.secretKey);
 
     const auth = {
       pubkey: enc(keypair.publicKey),
@@ -45,13 +45,13 @@ const self = module.exports = {
   },
   sign_message: (message) => {
     return nacl.sign.detached(
-      utils.str2ab(message), 
+      utility.str2ab(message), 
       keypair.secretKey
     );
   },
   hash_message: (message) => {
     return nacl.hash(
-      utils.str2ab(
+      utility.str2ab(
         JSON.stringify(message)
       )
     )
