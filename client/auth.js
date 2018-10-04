645a30a473aaa31f600d232348b8a7a87a4727b3
Guilherme Silva <guilhermefos.developer@gmail.com>
2018-10-04 11:06:25 -0300

    Refactor auth.js to use default Node module.exports

--- client/auth.js
+++ client/auth.js
@@ -1,64 +1,59 @@
-import nacl from 'tweetnacl';
-import util from 'tweetnacl-util';
-import querystring from 'querystring';
-
-import utils from './utils';
+const nacl = require('tweetnacl');
+const util = require('tweetnacl-util');
+const querystring = require('querystring');
+const utils = require('./utils');
 
 const enc = util.encodeBase64;
 const dec = util.decodeBase64;
 
 let keypair = {};
 
-export const generate_key = () => {
-  keypair = nacl.sign.keyPair();
-  return keypair.secretKey;
-};
-
-export const from_secretKey = (secretKey) => {
-  keypair = nacl.sign.keyPair.fromSecretKey(
-    dec(secretKey)
-  );
-};
-
-export const init_keychain = (secretKey) => {
-  if(secretKey) {
-    from_secretKey(secretKey);
-  } else {
-    generate_key();
-  }
-};
-
-export const authenticate = (secretKey) => {
-  init_keychain(secretKey);
-  const auth = {
-    pubkey: enc(keypair.publicKey),
-    message: sign_message(
-      enc(new Date())
-    ),
-    signature: enc(signature)
-  };
-  return querystring.stringify(auth)
-};
-
-export const sign_message = (message) => {
-  return nacl.sign.detached(
-    utils.str2ab(message), 
-    keypair.secretKey
-  );
-};
-
-export const hash_message = (message) => {
-  return nacl.hash(
-    utils.str2ab(
-      JSON.stringify(message)
+module.exports = () => ({
+  generate_key: () => {
+    keypair = nacl.sign.keyPair();
+    return keypair.secretKey;
+  },
+  from_secretKey: (secretKey) => {
+    keypair = nacl.sign.keyPair.fromSecretKey(
+      dec(secretKey)
+    );
+  },
+  init_keychain: (secretKey) => {
+    if(secretKey) {
+      from_secretKey(secretKey);
+    } else {
+      generate_key();
+    }
+  },
+  authenticate: (secretKey) => {
+    init_keychain(secretKey);
+    const auth = {
+      pubkey: enc(keypair.publicKey),
+      message: sign_message(
+        enc(new Date())
+      ),
+      signature: enc(signature)
+    };
+    return querystring.stringify(auth)
+  },
+  sign_message: (message) => {
+    return nacl.sign.detached(
+      utils.str2ab(message), 
+      keypair.secretKey
+    );
+  },
+  hash_message: (message) => {
+    return nacl.hash(
+      utils.str2ab(
+        JSON.stringify(message)
+      )
     )
-  )
-};
-
-export const sign_transaction = (transaction) => {
-  return enc(
-    sign_message(
-      hash_message(transaction)
-    )
-  );
-};
+  },
+  sign_transaction: (transaction) => {
+    return enc(
+      sign_message(
+        hash_message(transaction)
+      )
+    );
+  },
+});
