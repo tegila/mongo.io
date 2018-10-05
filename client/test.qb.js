7b702403b40fac4875065ca09656f2d2c7961574
Guilherme Silva <guilhermefos.developer@gmail.com>
2018-10-05 00:57:32 -0300

    Change to test update

--- client/test.qb.js
+++ client/test.qb.js
@@ -1,14 +1,14 @@
 const qb = require('./queryBuilder');
-const Store = require('./index');
 
-store = Store('http://localhost');
-store.connect('FKRz1v/vlFL3Y65LLxIZlxJTc4zD5jPUWLGzqC7BQZgTnxjkoGxPB6tLfi0KXU1u5q+zycqqBSjMhKSBUfvgWw==');
+const query = qb("app/test")
+                  .update()
+                  .currentDate(true)
+                  .increment('quantity', 2)
+                  .multiplay('quantity', 2)
+                  .rename('quantity', 'amount')
+                  .set('amount', 100)
+                  .setOnInsert('defaultQty', 100)
+                  .unset('user')
+                  .toJSON();
 
-store.on('connect', (session) => {
-  console.log('connected');
-  store.me().then(console.log);
-
-  store.on('test/session', (session) => {
-    console.log("GLOBAL [test.js] store.on: ", session);
-  });
-});
+console.log('update', query);
