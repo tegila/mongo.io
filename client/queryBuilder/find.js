0785290b9c77a2746d2f65af0a3aaddfedb9e2de
Guilherme Silva <guilhermefos.developer@gmail.com>
2018-10-05 14:37:16 -0300

    Add $ne to queryBuilder find.js
    $ne selects the documents where the value of the field is not equal to the specified value. This includes documents that do not contain the field.
    
    db.inventory.find( { qty: { $ne: 20 } } )

--- client/queryBuilder/find.js
+++ client/queryBuilder/find.js
@@ -37,6 +37,13 @@ const Find = (query) => ({
     payload = Object.assign({}, payload, { [field]: { $lte: value } });
     return Find(query);
   },
+  ne: (obj) => {
+    console.log('ne');
+    const field = Object.keys(obj)[0];
+    const value = Object.values(obj)[0];
+    payload = Object.assign({}, payload, { [field]: { $ne: value } });
+    return Find(query);
+  },
   in: (obj) => {
     console.log('in');
     const field = Object.keys(obj)[0];
