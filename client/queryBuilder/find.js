a937859ec5e4f0622f37781ccedda62afb455ac3
Guilherme Silva <guilhermefos.developer@gmail.com>
2018-10-08 11:35:21 -0300

    Refactor QueryBuilder find.js

--- client/queryBuilder/find.js
+++ client/queryBuilder/find.js
@@ -7,83 +7,61 @@ const payload_stringfy = (payload) => {
 const payload_parse = (payload) => {
   return JSON.parse(payload);
 }
-
+/**
+ * Query and Projection Operators
+ * See {@link https://docs.mongodb.com/manual/reference/operator/query/}
+ */
 const Find = (query) => ({
-  /** Query Selectors - Comparison */
-  eq: (obj) => {
+  /**
+   * Comparison Query Operators
+   * See {@link https://docs.mongodb.com/manual/reference/operator/query-comparison/}
+   */
+  eq: (field, value) => {
     console.log('eq');
-    const field = Object.keys(obj)[0];
-    const value = Object.values(obj)[0];
     payload = Object.assign({}, payload, { [field]: { $eq: value } });
     return Find(query);
   },
-  gt: (obj) => {
+  gt: (field, value) => {
     console.log('gt');
-    const field = Object.keys(obj)[0];
-    const value = Object.values(obj)[0];
     payload = Object.assign({}, payload, { [field]: { $gt: value } });
     return Find(query);
   },
-  gte: (obj) => {
+  gte: (field, value) => {
     console.log('gte');
-    const field = Object.keys(obj)[0];
-    const value = Object.values(obj)[0];
     payload = Object.assign({}, payload, { [field]: { $gte: value } });
     return Find(query);
   },
-  lt: (obj) => {
-    console.log('lt');
-    const field = Object.keys(obj)[0];
-    const value = Object.values(obj)[0];
-    payload = Object.assign({}, payload, { [field]: { $lt: value } });
-    return Find(query);
-  },
-  lte: (obj) => {
-    console.log('lte');
-    const field = Object.keys(obj)[0];
-    const value = Object.values(obj)[0];
-    payload = Object.assign({}, payload, { [field]: { $lte: value } });
-    return Find(query);
-  },
-  ne: (obj) => {
-    console.log('ne');
-    const field = Object.keys(obj)[0];
-    const value = Object.values(obj)[0];
-    payload = Object.assign({}, payload, { [field]: { $ne: value } });
-    return Find(query);
-  },
-  nin: (obj) => {
-    console.log('nin');
-    const field = Object.keys(obj)[0];
-    const value = Object.values(obj)[0];
-    payload = Object.assign({}, payload, { [field]: { $nin: value } });
-    return Find(query);
-  },
-  in: (obj) => {
+  in: (field, value) => {
     console.log('in');
-    const field = Object.keys(obj)[0];
-    const value = Object.values(obj)[0];
+    if (typeof value !== 'object') throw '$in needs an array';
     payload = Object.assign({}, payload, { [field]: { $in: value } });
     return Find(query);
   },
-  sort: (sort) => {
-    console.log('sort');
-    const field = Object.keys(sort)[0];
-    const value = Object.values(sort)[0];
-    payload = Object.assign({}, payload, { $sort:{ [field]: value } });
+  lt: (field, value) => {
+    console.log('lt');
+    payload = Object.assign({}, payload, { [field]: { $lt: value } });
     return Find(query);
   },
-  limit: (number) => {
-    console.log('limit');
-    payload = Object.assign({}, payload, { $limit: number });
+  lte: (field, value) => {
+    console.log('lte');
+    payload = Object.assign({}, payload, { [field]: { $lte: value } });
     return Find(query);
   },
-  skip: (number) => {
-    console.log('skip');
-    payload = Object.assign({}, payload, { $skip: number });
+  ne: (field, value) => {
+    console.log('ne');
+    payload = Object.assign({}, payload, { [field]: { $ne: value } });
     return Find(query);
   },
-  /** Logical Query Operators */
+  nin: (field, value) => {
+    console.log('nin');
+    if (typeof value !== 'object') throw '$nin needs an array';
+    payload = Object.assign({}, payload, { [field]: { $nin: value } });
+    return Find(query);
+  },
+  /**
+   * Logical Query Operators
+   * See {@link https://docs.mongodb.com/manual/reference/operator/query-logical/}
+   */
   not: (obj) => {
     console.log('not');
     const field = Object.keys(obj)[0];
@@ -91,9 +69,12 @@ const Find = (query) => ({
     payload = Object.assign({}, payload, { $not:{ [field]: value } });
     return Find(query);
   },
+  /**
+   * @returns JSON
+   */
   toJSON: () => {
     return JSON.stringify(payload);
-  }
+  },
 });
 
 module.exports = Find;
