039f02276ee045942e850ad99ff31518b41cf84d
Guilherme Silva <guilhermefos.developer@gmail.com>
2018-10-09 11:23:40 -0300

    Add log to connect

--- client/index.js
+++ client/index.js
@@ -21,8 +21,8 @@ module.exports = (url) => {
       });
     },
     connect: (secretKey) => {
-      const auth_enc = auth.authenticate(secretKey); 
-           
+      const auth_enc = auth.authenticate(secretKey);
+      console.log('[ CONNECT ] [ AUTH_ENC ]', auth_enc);
       // https://localhost/socket.io?query={signature=...&pubkey=...&message=...}
       io = socket.connect(url, { rejectUnauthorized: false, reconnect: true, query: auth_enc });
     },
