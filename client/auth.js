b33f490725873a932365e167f1d8c4f3fec2d332
Frederico F. de Oliveira <fredf_oliveira@ufu.br>
2018-10-09 02:46:33 -0300

    Change file to be exported as module

--- client/auth.js
+++ client/auth.js
@@ -1,64 +1,66 @@
-import nacl from 'tweetnacl';
-import util from 'tweetnacl-util';
-import querystring from 'querystring';
-
-import utils from './utils';
+const nacl = require('tweetnacl');
+const util = require('tweetnacl-util');
+const querystring = require('querystring');
+const utils = require('./utils');
 
 const enc = util.encodeBase64;
 const dec = util.decodeBase64;
 
 let keypair = {};
 
-export const generate_key = () => {
-  keypair = nacl.sign.keyPair();
-  return keypair.secretKey;
-};
+const self = module.exports = {
+  get_secretKey: () => {
+    return keypair.secretKey
+  },
+  generate_key: () => {
+    keypair = nacl.sign.keyPair();
+    return keypair.secretKey;
+  },
+  from_secretKey: (secretKey) => {
+    keypair = nacl.sign.keyPair.fromSecretKey(
+      dec(secretKey)
+    );
+  },
+  init_keychain: (secretKey) => {
+    if(secretKey) {
+      self.from_secretKey(secretKey);
+    } else {
+      self.generate_key();
+    }
+  },
+  authenticate: (secretKey) => {      
+    self.init_keychain(secretKey);
 
-export const from_secretKey = (secretKey) => {
-  keypair = nacl.sign.keyPair.fromSecretKey(
-    dec(secretKey)
-  );
-};
+    const message = new Date().toString();
+    const signature = nacl.sign.detached(utils.str2ab(message), keypair.secretKey);
 
-export const init_keychain = (secretKey) => {
-  if(secretKey) {
-    from_secretKey(secretKey);
-  } else {
-    generate_key();
-  }
-};
-
-export const authenticate = (secretKey) => {
-  init_keychain(secretKey);
-  const auth = {
-    pubkey: enc(keypair.publicKey),
-    message: sign_message(
-      enc(new Date())
-    ),
-    signature: enc(signature)
-  };
-  return querystring.stringify(auth)
-};
-
-export const sign_message = (message) => {
-  return nacl.sign.detached(
-    utils.str2ab(message), 
-    keypair.secretKey
-  );
-};
-
-export const hash_message = (message) => {
-  return nacl.hash(
-    utils.str2ab(
-      JSON.stringify(message)
+    const auth = {
+      pubkey: enc(keypair.publicKey),
+      message: self.sign_message(
+        enc(new Date().toString())
+      ),
+      signature: enc(signature)
+    };
+    return querystring.stringify(auth)
+  },
+  sign_message: (message) => {
+    return nacl.sign.detached(
+      utils.str2ab(message), 
+      keypair.secretKey
+    );
+  },
+  hash_message: (message) => {
+    return nacl.hash(
+      utils.str2ab(
+        JSON.stringify(message)
+      )
     )
-  )
-};
-
-export const sign_transaction = (transaction) => {
-  return enc(
-    sign_message(
-      hash_message(transaction)
-    )
-  );
+  },
+  sign_transaction: (transaction) => {
+    return enc(
+      self.sign_message(
+        self.hash_message(transaction)
+      )
+    );
+  },
 };
--- client/utils.js
+++ client/utils.js
@@ -1,22 +1,22 @@
-
-/* https://developer.mozilla.org/en-US/docs/Web/API/TextEncoder  */
-export const str2ab = (str) => {
-  const buf = new Uint8Array(str.length); // 2 bytes for each char
-  for (let i = 0; i < str.length; i += 1) {
-    buf[i] = str.charCodeAt(i);
-  }
-  return buf;
-}
-
-// https://stackoverflow.com/questions/12075927/serialization-of-regexp
-export const __parse_regex__ = (obj) => {
-  var key, value;
-  for (key in obj) {
-    value = obj[key];
-    if (value !== null && value instanceof RegExp) {
-      obj[key] = ("__REGEXP " + value.toString());
-    } else if (typeof value === 'object') {
-      __parse_regex__(value);
+  /* https://developer.mozilla.org/en-US/docs/Web/API/TextEncoder  */
+  /* https://stackoverflow.com/questions/12075927/serialization-of-regexp */
+const self = module.exports = {
+  str2ab: (str) => {
+    const buf = new Uint8Array(str.length); // 2 bytes for each char
+    for (let i = 0; i < str.length; i += 1) {
+      buf[i] = str.charCodeAt(i);
+    }
+    return buf;
+  },
+  __parse_regex__: (obj) => {
+    var key, value;
+    for (key in obj) {
+      value = obj[key];
+      if (value !== null && value instanceof RegExp) {
+        obj[key] = ("__REGEXP " + value.toString());
+      } else if (typeof value === 'object') {
+        self.__parse_regex__(value);
+      }
     }
   }
-}
+};
