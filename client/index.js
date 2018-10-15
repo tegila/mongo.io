909cd5833e7dd4589378fc1b9943aec70c2217b4
Guilherme Silva <guilhermefos.developer@gmail.com>
2018-10-15 13:14:29 -0300

    Change caller function name

--- client/index.js
+++ client/index.js
@@ -22,7 +22,7 @@ module.exports = (url) => {
       });
     },
     connect: (secretKey) => {
-      const auth_enc = auth.authenticate(secretKey);
+      const auth_enc = auth.auth_from_secretKey(secretKey);
       // https://localhost/socket.io?query={signature=...&pubkey=...&message=...}
       io = socket.connect(url, { rejectUnauthorized: false, reconnect: true, query: auth_enc });
     },
